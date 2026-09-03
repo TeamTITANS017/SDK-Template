cc3123c06d44bed24a2cf5b897ce231294011aac
PepeneleZ <pepegame96@gmail.com>
2026-09-03 22:27:58 +0300

    Completat structura

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleOpTemplate.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TeleOpTemplate extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
