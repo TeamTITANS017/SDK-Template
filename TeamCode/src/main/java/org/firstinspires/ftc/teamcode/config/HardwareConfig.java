76b3f5e6727cdf34d71650ab224852bea7f2d6da
PepeneleZ <pepegame96@gmail.com>
2026-09-03 20:07:15 +0300

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/HardwareConfig.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.config;
+
+public class HardwareConfig { // Clasa care sa contina denumirile din config
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/RobotConstants.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.config;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class RobotConstants { // Clasa care sa contina diferite constante sub forma de public enums
+}
